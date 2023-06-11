eed3fe0fadab53f94c17560a24174d123cd02ae8
Yury Uvarov <uvarov.frontend@gmail.com>
2023-06-11 18:32:05 +0300

    add scroll-hidden

--- src/app/components/Advantages.tsx
+++ src/app/components/Advantages.tsx
@@ -11,28 +11,30 @@ export default async function Advantages() {
   return (
     <section className="py-5 lg:py-8">
       <h2 className="sr-only">Преимущества</h2>
-      <ul className="grid grid-flow-row gap-5 sm:grid-cols-2 xl:gap-7 xl:grid-cols-4">
-        {advantages?.map((advantage) => (
-          <li
-            key={advantage.id}
-            className="relative rounded-2xl bg-white px-5 py-6 pl-[100px] shadow-sm transition-all before:absolute
-            before:left-5 before:top-1/2 before:h-[60px] before:w-[60px] before:-translate-y-1/2 before:rounded-full before:bg-green before:opacity-30 hover:shadow-md"
-          >
-            <Image
-              alt={advantage.attributes.icon.data.attributes.name}
-              className="absolute left-[38px] top-1/2 block h-6 w-6 -translate-y-1/2"
-              height={24}
-              src={`${process.env.STRAPI_API_URL}${advantage.attributes.icon.data.attributes.url}`}
-              width={24}
-            />
-            <b className="mb-1 block font-medium">{advantage.attributes.title}</b>
-            <span className="block text-sm text-dark_gray">
-              {/* @ts-expect-error Server Component */}
-              <MDXRemote source={advantage.attributes.content} />
-            </span>
-          </li>
-        ))}
-      </ul>
+      <div className="scroll-hidden">
+        <ul className="grid grid-flow-row gap-5 xl:gap-7 grid-cols-[repeat(4,_284px)] w-max xl:grid-cols-4 xl:w-full">
+          {advantages?.map((advantage) => (
+            <li
+              key={advantage.id}
+              className="relative rounded-2xl bg-white px-5 py-6 pl-[100px] shadow-sm transition-all before:absolute
+              before:left-5 before:top-1/2 before:h-[60px] before:w-[60px] before:-translate-y-1/2 before:rounded-full before:bg-green before:opacity-30 hover:shadow-md"
+            >
+              <Image
+                alt={advantage.attributes.icon.data.attributes.name}
+                className="absolute left-[38px] top-1/2 block h-6 w-6 -translate-y-1/2"
+                height={24}
+                src={`${process.env.STRAPI_API_URL}${advantage.attributes.icon.data.attributes.url}`}
+                width={24}
+              />
+              <b className="mb-1 block font-medium">{advantage.attributes.title}</b>
+              <span className="block text-sm text-dark_gray">
+                {/* @ts-expect-error Server Component */}
+                <MDXRemote source={advantage.attributes.content} />
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 }
