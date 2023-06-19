3ca4e97aafb579563675d82e32c13cfcc8356814
Yury Uvarov <uvarov.frontend@gmail.com>
2023-06-20 00:34:13 +0300

    add ya metrika

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,24 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             defer
             src="//code.jivo.ru/widget/5dswZerdPY"
             strategy="lazyOnload"
+
           />
+          <script type="text/javascript" dangerouslySetInnerHTML={{
+              __html: `
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+              ym(52988986, "init", {
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true
+              });
+              `,
+            }} />
         </body>
       </html>
     </ReCaptchaProviders>
