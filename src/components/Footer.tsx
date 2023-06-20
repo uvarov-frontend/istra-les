9d4f062c79e1b170d975c2636e386b51a92c4c3a
Yury Uvarov <uvarov.frontend@gmail.com>
2023-06-20 11:30:14 +0300

    add author

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -16,7 +16,7 @@ export default async function Footer() {
   if (!mainMenu || !otherMenu) return notFound();
 
   return (
-    <footer className="relative z-10 bg-lite py-7">
+    <footer className="relative z-10 bg-lite pt-7 pb-2">
       <div className="container mx-auto grid auto-cols-auto grid-flow-row grid-cols-1 gap-8 sm:grid-cols-2 md:grid-flow-col md:grid-cols-[1fr_1fr_auto] md:gap-6 lg:grid-cols-[auto_1fr_1fr_auto] lg:gap-16 xl:grid-cols-[auto_auto_auto_auto]">
         <div className="hidden lg:block">
           <Link className="block" href="/">
@@ -50,7 +50,10 @@ export default async function Footer() {
         </div>
         <Contacts title={contacts.contacts} />
       </div>
-      <small className="absolute bottom-2 right-2 text-xs text-gray_dark/60">{contacts.recaptcha}</small>
+      <div className="container mx-auto mt-7 flex justify-between md:items-center flex-col md:flex-row">
+        <small className="block mb-2 md:mb-0 text-gray_dark">Дизайн и разработка <a className="text-green hover:text-green_hover" href="https://github.com/uvarov-frontend" rel="noreferrer" target="_blank">Юрий Уваров</a>.</small>
+        <small className="block text-xs text-gray_dark/60">{contacts.recaptcha}</small>
+      </div>
     </footer>
   );
 }
